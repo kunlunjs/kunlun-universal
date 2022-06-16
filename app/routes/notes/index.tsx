53b6e01c147a769de801212996514840fc3750cf
Yudong Zhu <zhuyudong@aliyun.com>
2022-06-16 23:48:02 +0800

    perf: Improve '/notes' page style

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -98,45 +98,48 @@ export default function NotesRoute() {
               </p>
             </div>
           </div>
-          <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
             {notes.map(({ name, matter }, ix) => {
               return (
                 <Link key={matter.title} to={`/notes/${name?.split('.')[0]}`}>
-                  <blockquote className="flex h-full flex-col justify-between bg-white p-12">
-                    <div>
-                      <div className="flex justify-between">
-                        <span className="flex space-x-0.5 text-orange-400">
-                          {[...Array(matter.star || 0)].map((_, inx) => {
-                            return (
-                              <svg
-                                key={`${ix}.${inx}`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                              >
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                              </svg>
-                            )
-                          })}
-                        </span>
-                        <span className="text-xs text-gray-400">
-                          {matter.views} views
-                        </span>
+                  <blockquote className="group relative block h-full">
+                    <span className="absolute inset-0 rounded-md border border-dashed border-gray-400"></span>
+                    <div className="flex h-full flex-col justify-between rounded-md border border-gray-400 bg-white p-6 transition group-hover:-translate-x-2 group-hover:-translate-y-2">
+                      <div>
+                        <div className="flex justify-between">
+                          <span className="flex space-x-0.5 text-orange-400">
+                            {[...Array(matter.star || 0)].map((_, inx) => {
+                              return (
+                                <svg
+                                  key={`${ix}.${inx}`}
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="h-6 w-6"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                >
+                                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                </svg>
+                              )
+                            })}
+                          </span>
+                          <span className="text-xs text-gray-400">
+                            {matter.views} views
+                          </span>
+                        </div>
+                        <div className="mt-4">
+                          <h6 className="text-base font-bold text-pink-600 sm:text-lg">
+                            {matter.title}
+                          </h6>
+                          <p className="mt-2 text-sm text-gray-600">
+                            {matter.introduction}
+                          </p>
+                        </div>
                       </div>
-                      <div className="mt-4">
-                        <h6 className="text-base font-bold text-pink-600 sm:text-lg">
-                          {matter.title}
-                        </h6>
-                        <p className="mt-2 text-sm text-gray-600">
-                          {matter.introduction}
-                        </p>
-                      </div>
+                      <footer className="mt-8 flex justify-between text-xs text-gray-500">
+                        <span>{matter.author as string}</span>
+                        <span>{matter.publishAt}</span>
+                      </footer>
                     </div>
-                    <footer className="mt-8 flex justify-between text-xs text-gray-500">
-                      <span>{matter.author as string}</span>
-                      <span>{matter.publishAt}</span>
-                    </footer>
                   </blockquote>
                 </Link>
               )
